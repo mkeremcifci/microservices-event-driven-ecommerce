9c6e76582d861faa13daaba8dbe362c28e9bfe91
mkeremcifci <cifcimustafakerem@gmail.com>
2026-02-13 16:26:19 +0300

    feat: initialize user-service with basic NestJS application structure, configuration, and a TypeORM User entity.

---
+++ backend/user-service/src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+
+@Module({
+  imports:[
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [User],
+        synchronize: true,
+    }),
+    inject: [ConfigService],
+    }),
+  ]
+})
+export class AppModule {}
